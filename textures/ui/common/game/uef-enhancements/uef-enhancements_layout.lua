1512b03f78aa9303353de1a9369cfa10781c6bb4
Isakentyi <kfat2@mail.ru>
2025-04-29 19:00:01 +0300

    ACU aeon and UEF rebalance. Uef POD rebalance. New icons for UEF com.

---
+++ textures/ui/common/game/uef-enhancements/uef-enhancements_layout.lua
@@ -0,0 +1,10 @@
+layout = {
+	['hamc2_btn_sel'] = {left = 1, top = 0, width = 63, height = 64, },
+	['hamc2_btn_down'] = {left = 2, top = 2, width = 60, height = 60, },
+	['hamc2_btn_over'] = {left = 2, top = 2, width = 60, height = 60, },
+	['hamc2_btn_up'] = {left = 2, top = 2, width = 60, height = 60, },
+	['dse_btn_sel'] = {left = 1, top = 0, width = 63, height = 64, },
+	['dse_btn_down'] = {left = 2, top = 2, width = 60, height = 60, },
+	['dse_btn_over'] = {left = 2, top = 2, width = 60, height = 60, },
+	['dse_btn_up'] = {left = 2, top = 2, width = 60, height = 60, },
+	}
