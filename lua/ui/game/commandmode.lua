43d80eec76a271731b0f57c2d35f4e3d42274cec
Isakentyi <kfat2@mail.ru>
2025-05-28 01:33:51 +0300

    fix hotbuilding orders

--- lua/ui/game/commandmode.lua
+++ lua/ui/game/commandmode.lua
@@ -16,7 +16,6 @@ local EnhancementQueueFile = import("/lua/ui/notify/enhancementqueue.lua")
 
 local GameMain = import("/lua/ui/game/gamemain.lua")
 local RadialDragger = import("/lua/ui/controls/draggers/radial.lua").RadialDragger
-local AbortNavigation = import("/lua/keymap/misckeyactions.lua").AbortNavigation
 
 -- upvalue globals for performance
 local IsKeyDown = IsKeyDown
@@ -530,35 +529,6 @@ end
 
 ---@param command UserCommand
 local function OnGuardIssued(command)
-    local autoAbortNavigation = Prefs.GetOption("auto_abort_navigation")
-	if autoAbortNavigation == nil then
-        autoAbortNavigation = true
-    end
-	
-    WARN("OnGuardIssued is called!")
-	if autoAbortNavigation then
-	  if not OnGuardIssued.lastCalled or GetSystemTimeSeconds() - OnGuardIssued.lastCalled > 0.1 then
-			OnGuardIssued.lastCalled = GetSystemTimeSeconds()
-			ForkThread(function()
-            WaitSeconds(0.1)
-			local unit = GetUnitById(command.Target.EntityId) ---@cast unit UserUnit
-            local guards = command.Units
-            if unit and guards then
-                for _, guard in ipairs(guards) do
-                    if guard then
-                        local distance = VDist3(unit:GetPosition(), guard:GetPosition())
-                        local engineerRange = 17 
-                        if distance <= engineerRange then
-                            SimCallback({ Func = 'AbortNavigation', Args = {} }, true)
-                            break 
-                        end
-                    end
-                end
-            end
-			end)
-		end	
-	end 
-
     if command.Target.EntityId then
         local unit = GetUnitById(command.Target.EntityId) ---@cast unit UserUnit
         local guards = command.Units
@@ -621,10 +591,11 @@ end
 
 ---@param command UserCommand
 local function OnReclaimIssued(command)
-    -- Area reclaim dragger, command mode only
-    if command.Target.EntityId and modeData.name == "RULEUCC_Reclaim" then
-        import("/lua/ui/game/hotkeys/area-reclaim-order.lua").AreaReclaimOrder(command)
-    end
+    -- feature: area commands
+    -- -- Area reclaim dragger, command mode only
+    -- if command.Target.EntityId and modeData.name == "RULEUCC_Reclaim" then
+    --     import("/lua/ui/game/hotkeys/area-reclaim-order.lua").AreaReclaimOrder(command)
+    -- end
 end
 
 ---@param command UserCommand
@@ -638,6 +609,7 @@ end
 
 ---@param command UserCommand
 local function OnAttackIssued(command)
+    -- feature: area commands
     -- Area attack dragger, command mode only
     if command.Target.Type == 'Position' and modeData.name == "RULEUCC_Attack" then
         import("/lua/ui/game/hotkeys/area-attack-order.lua").AreaAttackOrder(command)
@@ -712,16 +684,21 @@ local OnCommandIssuedCallback = {
 -- @param command Information surrounding the command that has been issued, such as its CommandType or its Target.
 ---@param command UserCommand
 function OnCommandIssued(command)
+    if not command.Clear then
+        -- ??? signal for OnCommandModeBeat
+        issuedOneCommand = true
+    end
 
     -- If our callback returns true or we don't have a command type, we skip the rest of our logic
     if (OnCommandIssuedCallback[command.CommandType] and OnCommandIssuedCallback[command.CommandType](command))
     or command.CommandType == 'None' then
+        if command.Clear then
+            EndCommandMode(true)
+        end
         return
     end
     -- is set when we hold shift, to queue up multiple commands. This is where the command mode stops
-    if not command.Clear then
-        issuedOneCommand = true
-    else
+    if command.Clear then
         EndCommandMode(true)
         if command.CommandType ~= 'Stop'
         and TableGetN(command.Units) == 1
