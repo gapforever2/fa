3428461ef99ed738edeb727209c33929dc864754
llN <lolkovich228gg@bk.ru>
2025-10-18 17:22:18 +0300

    Projectiles UEF ACU Gun DeeKey

---
+++ projectiles/IonizedPlasmaCanon01/IonizedPlasmaCanon01_script.lua
@@ -0,0 +1,13 @@
+--****************************************************************************
+--**
+--**  File     :  /data/projectiles/TDFIonizedPlasmaGatlingCannon01/TDFIonizedPlasmaGatlingCannon01_script.lua
+--**  Author(s):  Gordon Duclos
+--**
+--**  Summary  :  UEF Ionized Plasma Gatling Cannon Projectile script, XEL0305
+--**
+--**  Copyright � 2007 Gas Powered Games, Inc.  All rights reserved.
+--****************************************************************************
+
+IonizedPlasmaCanon01 = Class(import('/lua/terranprojectiles.lua').TIonizedPlasmaGatlingCannon) {
+}
+TypeClass = IonizedPlasmaCanon01
