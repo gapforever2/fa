9565ad6a2b14b93ce1ba68a58cae81e95925b5c4
Angel_fugu <oresinaolesa@gmail.com>
2025-10-18 17:44:42 +0600

    AEON AIR FACTORY T2 HQ

--- units/UAB0202/UAB0202_script.lua
+++ units/UAB0202/UAB0202_script.lua
@@ -8,8 +8,7 @@
 --**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
 --****************************************************************************
 local AAirFactoryUnit = import("/lua/aeonunits.lua").AAirFactoryUnit
-
 ---@class UAB0202 : AAirFactoryUnit
-UAB0202 = ClassUnit(AAirFactoryUnit) {}
+UAB0202 = Class(AAirFactoryUnit) {}
 
 TypeClass = UAB0202
