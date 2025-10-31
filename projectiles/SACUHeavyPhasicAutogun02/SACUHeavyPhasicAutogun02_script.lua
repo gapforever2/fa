86914dca1851534492985ddfe79fbdb79fe36f4e
llN <lolkovich228gg@bk.ru>
2025-10-31 23:12:32 +0300

    Fix SACU x2

---
+++ projectiles/SACUHeavyPhasicAutogun02/SACUHeavyPhasicAutogun02_script.lua
@@ -0,0 +1,10 @@
+-- File     :  /data/projectiles/SDFHeavyPhasicAutogun02/SDFHeavyPhasicAutogun02_script.lua
+-- Author(s):  Gordon Duclos
+-- Summary  :  Heavy Phasic Autogun Projectile script, XSA0203
+-- Copyright © 2007 Gas Powered Games, Inc.  All rights reserved.
+---------------------------------------------------------------------------------------------
+
+--- Heavy Phasic Autogun Projectile script, XSA0203
+---@class SDFHeavyPhasicAutogun02 : SHeavyPhasicAutogun02
+SDFHeavyPhasicAutogun02 = ClassProjectile(import("/lua/seraphimprojectiles.lua").SHeavyPhasicAutogun02) {}
+TypeClass = SDFHeavyPhasicAutogun02
