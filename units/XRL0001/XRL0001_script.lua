4033f68d564aea95e9520572faa990ed541fcf46
Armagedon <testbotsarma@mail.ru>
2026-01-17 13:15:08 +0300

    Fix Cybran EXP Megalith Build Factory

---
+++ units/XRL0001/XRL0001_script.lua
@@ -0,0 +1,15 @@
+--****************************************************************************
+--**
+--**  File     :  units/XRL0001/XRL0001_script.lua
+--**
+--**  Summary  :  Crab egg
+--**
+--**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
+--****************************************************************************
+
+local CConstructionEggUnit = import("/lua/cybranunits.lua").CConstructionEggUnit
+
+---@class XRL0002 : CConstructionEggUnit
+XRL0001 = ClassUnit(CConstructionEggUnit) {}
+
+TypeClass = XRL0001
