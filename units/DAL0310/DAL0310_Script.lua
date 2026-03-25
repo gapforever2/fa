8c3c32f22df8f37353a1c4917c4a01ef91289c7c
Angel_fugu <oresinaolesa@gmail.com>
2026-03-25 16:37:28 +0600

    fix bug panch 11
    исправлены баги потерявшихся комитов

--- units/DAL0310/DAL0310_Script.lua
+++ units/DAL0310/DAL0310_Script.lua
@@ -1,28 +1,18 @@
-#****************************************************************************
-#**
-#**  File     :  /cdimage/units/DAL0310/DAL0310_script.lua
-#**  Author(s):  Dru Staltman, Matt Vainio
-#**
-#**  Summary  :  Aeon Shield Disruptor Script
-#**
-#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
-#****************************************************************************
+------------------------------------------------------------------------------
+-- File     :  /cdimage/units/DAL0310/DAL0310_script.lua
+-- Author(s):  Dru Staltman, Matt Vainio
+-- Summary  :  Aeon Shield Disruptor Script
+-- Copyright Â© 2005 Gas Powered Games, Inc.  All rights reserved.
+------------------------------------------------------------------------------
 
-#changes from ALandUnit to AHoverLandUnit
-local AHoverLandUnit = import('/lua/aeonunits.lua').AHoverLandUnit
-local ADFDisruptorCannonWeapon = import('/lua/aeonweapons.lua').ADFDisruptorWeapon
+--changes from ALandUnit to AHoverLandUnit
+local AHoverLandUnit = import("/lua/aeonunits.lua").AHoverLandUnit
+local ADFDisruptorCannonWeapon = import("/lua/aeonweapons.lua").ADFDisruptorWeapon
 
-DAL0310 = Class(AHoverLandUnit) {
+---@class DAL0310 : AHoverLandUnit
+DAL0310 = ClassUnit(AHoverLandUnit) {
     Weapons = {
-        MainGun = Class(ADFDisruptorCannonWeapon) {
-            CreateProjectileAtMuzzle = function(self, muzzle)
-                local proj = ADFDisruptorCannonWeapon.CreateProjectileAtMuzzle(self, muzzle)
-                local data = self:GetBlueprint().DamageToShields
-                if proj and not proj:BeenDestroyed() then
-                    proj:PassData(data)
-                end
-            end,
-          }
+        MainGun = ClassWeapon(ADFDisruptorCannonWeapon) {}
     },
 }
 TypeClass = DAL0310
