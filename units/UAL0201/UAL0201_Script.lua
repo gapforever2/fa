36aeae7f1147aae6251e34fbaa392882fda19d39
Angel_fugu <oresinaolesa@gmail.com>
2026-03-29 16:37:22 +0600

    Reapply "Merge branch 'GAP-1-Release' of github.com:gapforever2/fa into GAP-1-Release"
    This reverts commit 0b9dd70cd1ab7d9d96ad8d78a739167d785ff12d.

--- units/UAL0201/UAL0201_Script.lua
+++ units/UAL0201/UAL0201_Script.lua
@@ -1,19 +1,20 @@
-#****************************************************************************
-#**
-#**  File     :  /cdimage/units/UAL0201/UAL0201_script.lua
-#**  Author(s):  John Comes, David Tomandl, Jessica St. Croix
-#**
-#**  Summary  :  Aeon Light Tank Script
-#**
-#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
-#****************************************************************************
+--****************************************************************************
+--**
+--**  File     :  /cdimage/units/UAL0201/UAL0201_script.lua
+--**  Author(s):  John Comes, David Tomandl, Jessica St. Croix
+--**
+--**  Summary  :  Aeon Light Tank Script
+--**
+--**  Copyright Â© 2005 Gas Powered Games, Inc.  All rights reserved.
+--****************************************************************************
 
-local AHoverLandUnit = import('/lua/aeonunits.lua').AHoverLandUnit
-local ADFDisruptorCannonWeapon = import('/lua/aeonweapons.lua').ADFDisruptorCannonWeapon
+local AHoverLandUnit = import("/lua/aeonunits.lua").AHoverLandUnit
+local ADFDisruptorCannonWeapon = import("/lua/aeonweapons.lua").ADFDisruptorCannonWeapon
 
-UAL0201 = Class(AHoverLandUnit) {
+---@class UAL0201 : AHoverLandUnit
+UAL0201 = ClassUnit(AHoverLandUnit) {
     Weapons = {
-        MainGun = Class(ADFDisruptorCannonWeapon) {}
+        MainGun = ClassWeapon(ADFDisruptorCannonWeapon) {}
     },
 }
 TypeClass = UAL0201
