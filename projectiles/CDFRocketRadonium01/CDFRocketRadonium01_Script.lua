517889f72e0b40dce67e47dd3962f53681c79410
llN <lolkovich228gg@bk.ru>
2026-08-21 15:26:32 +0300

    T2 Bots (Mangust, Goplit)
    Гоплит: Добавлен клон основного орудия. Иридиума (Обычный), Радониум (0 урона, но по щитам урон 200 с дпсом 150 по щитам).
    Мангуст: Добавлен вижен от гранат как у т1 артиллерии ОФЗ.

---
+++ projectiles/CDFRocketRadonium01/CDFRocketRadonium01_Script.lua
@@ -0,0 +1,12 @@
+------------------------------------------------------------
+--  File     :  /data/projectiles/CDFRocketRadonium01/CDFRocketRadonium01_Script.lua
+--  Author(s):  Matt Vainio
+--  Summary  :  Cybran Radonium Rocket Tubes, DRL0204 : cyb T2 range bot (hoplite)
+--  Copyright © 2007 Gas Powered Games, Inc.  All rights reserved.
+------------------------------------------------------------
+local CIridiumRocketProjectile = import("/lua/cybranprojectiles.lua").CIridiumRocketProjectile
+
+--- Cybran Radonium Rocket Tubes, DRL0204 : cyb T2 range bot (hoplite)
+---@class CDFRocketRadonium01 : CIridiumRocketProjectile
+CDFRocketRadonium01 = ClassProjectile(CIridiumRocketProjectile) { }
+TypeClass = CDFRocketRadonium01
--- units/DRL0204/DRL0204_script.lua
+++ units/DRL0204/DRL0204_script.lua
@@ -12,6 +12,7 @@
 DRL0204 = ClassUnit(import("/lua/cybranunits.lua").CWalkingLandUnit) {
     Weapons = {
         RocketBackpack = ClassWeapon(import("/lua/cybranweapons.lua").CDFRocketIridiumWeapon02) {},
+        RadoniumBackpack = ClassWeapon(import("/lua/cybranweapons.lua").CDFRocketIridiumWeapon02) {},
     },
 }
 TypeClass = DRL0204
