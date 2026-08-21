15bc9e0bf174ab28a1e8f0574b835a7aced40026
llN <lolkovich228gg@bk.ru>
2026-08-21 15:39:23 +0300

    Add vision UEL0304
    Добавлен вижен от выстрела. =5.

--- projectiles/TIFAntiMatterShells02/TIFAntiMatterShells02_script.lua
+++ projectiles/TIFAntiMatterShells02/TIFAntiMatterShells02_script.lua
@@ -1,4 +1,5 @@
 local TArtilleryAntiMatterSmallProjectile = import("/lua/terranprojectiles.lua").TArtilleryAntiMatterSmallProjectile
+local VisionMarkerOpti = import("/lua/sim/vizmarker.lua").VisionMarkerOpti
 
 -- UEF T3 Mobile Artillery Anti-Matter Shells : uel0304
 ---@class TIFAntiMatterShells02: TArtilleryAntiMatterSmallProjectile
@@ -8,6 +9,12 @@ TIFAntiMatterShells02 = ClassProjectile(TArtilleryAntiMatterSmallProjectile) {
     ---@param targetType string
     ---@param targetEntity Prop|Unit
     OnImpact = function(self, targetType, targetEntity)
+        local px, _, pz = self:GetPositionXYZ()
+        local marker = VisionMarkerOpti({ Owner = self })
+        marker:UpdatePosition(px, pz)
+        marker:UpdateDuration(5)
+        marker:UpdateIntel(self.Army, 5, 'Vision', true)
+
         TArtilleryAntiMatterSmallProjectile.OnImpact(self, targetType, targetEntity)
         self:ShakeCamera( 20, 1, 0, 1 )
     end,
